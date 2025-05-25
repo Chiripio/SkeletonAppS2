65292e8652afde33dfa4539f018a85d3bdac41b9
Chiripio <ed.guerreros@duocuc.cl>
2025-05-25 16:10:30 -0400

    Mejora visual en página de login: centrado, estilos limpios y campos mejorados

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,12 +8,8 @@ const routes: Routes = [
   },
   {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'login',
     pathMatch: 'full'
-  },
-  {
-    path: 'login',
-    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
   },
   {
     path: 'login',
